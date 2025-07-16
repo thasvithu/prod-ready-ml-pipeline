bb51216bf605b16f0ca8ca49f729e35ae1cbe95c
thasvithu <thasvithu7@gmail.com>
2025-07-16 16:35:25 +0530

    logging added

--- noteboks/test2.py
+++ noteboks/test2.py
@@ -3,10 +3,9 @@ import sys
 
 def test_custom_exception():
     try:
-        # Intentionally causing a division by zero error
         x = 10 / 0
     except Exception as e:
-        # Raising CustomException with the caught exception details
-        raise CustomException("Something went wrong!", sys) from e
+        error = CustomException("Something went wrong!", sys)
+        print("Custom Error:", error)
     
 test_custom_exception()
---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from datetime import datetime
+
+# Generate log filename
+LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+
+# Just the directory (no filename)
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# Full path to log file
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# Setup logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
